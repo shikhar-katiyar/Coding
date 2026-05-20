fa4c0852e0c891ee83021f942910ea0b32b31420
shikhar-katiyar <115767272+shikhar-katiyar@users.noreply.github.com>
2026-05-20 11:00:34 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (15.47%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,17 +1,17 @@
 class Solution {
-    public void rotate(int[][] mat) {
-        for (int i=0; i<mat.length; i++){
-            for (int j=i+1; j<mat.length; j++) {
-                int temp = mat[i][j];
-                mat[i][j]=mat[j][i];
-                mat[j][i]= temp;
+    public void rotate(int[][] matrix) {
+        for (int i=0; i<matrix.length; i++){
+            for (int j=0; j<i; j++) {
+                int temp=matrix[i][j];
+                matrix[i][j]= matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
-        for (int i=0; i<mat.length; i++){
-            for (int j=0; j<mat.length/2; j++) {
-                int temp = mat[i][j];
-                mat[i][j]=mat[i][mat[0].length-1-j];
-                mat[i][mat.length-1-j]= temp;
+        for (int i=0; i<matrix.length; i++){
+            for (int j=0; j<matrix.length/2; j++) {
+                int temp=matrix[i][j];
+                matrix[i][j]= matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j]=temp;
             }
         }
     }
