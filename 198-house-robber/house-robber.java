4165f01118f54488edd3f9958c2ab6c8261f853a
shikhar-katiyar <115767272+shikhar-katiyar@users.noreply.github.com>
2026-09-02 10:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (94.65%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,15 +1,15 @@
-import java.util.Arrays; 
 class Solution {
     public int rob(int[] nums) {
         int[] memo= new int[nums.length];
         Arrays.fill(memo, -1);
-        return ro(nums, 0, memo);
+        return f(memo,nums, 0);
     }
-    private int ro(int[] nums, int index, int[] memo) {
+    int f(int[] memo, int[] nums, int index) {
         if (index>=nums.length) return 0;
-        if (memo[index] != -1) return memo[index];
-        int select= nums[index] + ro(nums, index +2, memo);
-        int skip= ro(nums, index +1, memo);
-        return memo[index]= Math.max(select, skip);
+        if (memo[index]!=-1) return memo[index];
+        int select = nums[index] + f(memo, nums, index+2);
+        int skip= f(memo, nums, index+1);
+        memo[index]=Math.max(select, skip);
+        return memo[index];
     }
 }
