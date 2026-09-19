db71891c00d8bb2b1f73eb9a5bce3b59c87037d5
shikhar-katiyar <115767272+shikhar-katiyar@users.noreply.github.com>
2026-09-19 10:30:50 +0530

    Time: 7 ms (82.10%) | Memory: 45.7 MB (95.98%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -8,11 +8,16 @@ class Solution {
             if (temp.size() == 0 || nums[i] > temp.get(temp.size() - 1)) {
                 temp.add(nums[i]);
             } else {
-                int p = 0;
-                while (temp.get(p) < nums[i]) {
-                    p++;
+                int low=0;
+                int high = temp.size()-1;
+                while (low<=high) {
+                    int mid=(low+high)/2;
+                    if (temp.get(mid)>= nums[i]) {
+                        high=mid-1;
+                    }
+                    else low=mid+1;
                 }
-                temp.set(p, nums[i]);
+                temp.set(low, nums[i]);
             }
         }
 
